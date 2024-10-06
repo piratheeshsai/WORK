9992dfe9a9e5a2dd5dfec26b56a0c3a3facb6e12
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2024-10-06 12:32:06 +0530

    image link update

--- resources/views/admin/component/menu.blade.php
+++ resources/views/admin/component/menu.blade.php
@@ -4,8 +4,7 @@ id="sidenav-main">
 <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
         aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
-        target="_blank">
+    <a class="navbar-brand m-0" href=" {{ route('admin.dashboard') }}">
         <img src="{{ asset('img/logo-ct-dark.png') }}" alt="Logo" class="navbar-brand-img w-100 h-100"
             alt="main_logo">
         <span class="ms-5 font-weight-bold">WOMS</span>
