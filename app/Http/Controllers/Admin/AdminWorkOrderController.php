08cfa35a50fffb3190f20e4531553aa888bf8da2
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2024-11-01 19:20:45 +0530

    Updated

--- app/Http/Controllers/Admin/AdminWorkOrderController.php
+++ app/Http/Controllers/Admin/AdminWorkOrderController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\UserDetail;
 use App\Models\WorkOrder;
-use Illuminate\Http\Request;
+
 
 class AdminWorkOrderController extends Controller
 {
