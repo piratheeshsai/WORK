487959727c9b7e3a3a8b432d40565ad0b5c53da6
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2024-11-01 23:18:29 +0530

    Civil Dashboard

---
+++ app/Http/Controllers/Civil/CivilWorkOrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Civil;
+
+use App\Http\Controllers\Controller;
+use App\Models\workOrder;
+use Illuminate\Http\Request;
+
+class CivilWorkOrderController extends Controller
+{
+    public function index()
+    {
+        $workOrders = WorkOrder::where('status', 'approved')
+                               ->where('work_type', 'civil')
+                               ->paginate(10);
+        return view('civil.workOrder.index', compact('workOrders'));
+    }
+}
--- resources/views/electrical/component/menu.blade.php
+++ resources/views/electrical/component/menu.blade.php
@@ -15,7 +15,7 @@ id="sidenav-main">
 <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link active" href="{{ route('recommender.dashboard') }}">
+            <a class="nav-link active" href="{{ route('electrical.dashboard') }}">
                 <div
                     class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                     <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
