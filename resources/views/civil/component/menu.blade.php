e462cc0cd22f11eda5a1ea467afdf75eaf566cdf
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2024-11-03 09:04:26 +0530

    updated

--- resources/views/civil/component/menu.blade.php
+++ resources/views/civil/component/menu.blade.php
@@ -33,7 +33,7 @@ id="sidenav-main">
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link " href="{{ route('recommender.work.index') }}">
+            <a class="nav-link " href="{{ route('civil.work.index') }}">
                 <div
                     class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                     <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
