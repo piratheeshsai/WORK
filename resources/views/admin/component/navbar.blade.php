d16d50af61b6c7b643e1f21cc9665b7be04ad4b8
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2025-03-29 06:55:23 +0530

    in

--- resources/views/admin/component/navbar.blade.php
+++ resources/views/admin/component/navbar.blade.php
@@ -9,7 +9,7 @@
         <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
     </nav>
     <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-        <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+        <div class="ms-md-auto pe-md-3 d-flex align-items-center" method="get">
             <div class="input-group">
                 <span class="input-group-text text-body"><i class="fas fa-search"
                         aria-hidden="true"></i></span>
